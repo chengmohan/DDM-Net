e14aad46893486ee9bfa8712da76ee498941da97
chengmohan <144802767+chengmohan@users.noreply.github.com>
2023-12-07 11:29:48 +0800

    Update dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -11,7 +11,7 @@ from random import randint
 #判断图像的扩展名是不是.png,.jpg,.jpeg,.tif
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg", ".tif"])
-
+#将tiff文件读取，最终读取成ndarray类型
 def load_img(filepath):
     # img = Image.open(filepath+'/1.tif')
     # y = np.array(img).reshape(1,img.size[0],img.size[1])
@@ -22,7 +22,7 @@ def load_img(filepath):
     img = picture[0].transpose(2, 1, 0)
     # img_test = Image.fromarray(img[:,:,1])
     return img
-
+#随机裁剪将图片a从随机位置裁剪为crop_size*crop_size大小的图片
 def randcrop(a, crop_size):
     [wid, hei, nband]=a.shape
     crop_size1 = crop_size
@@ -30,11 +30,12 @@ def randcrop(a, crop_size):
     Height = random.randint(0, hei - crop_size1 - 1)
 
     return a[Width:(Width + crop_size1),  Height:(Height + crop_size1), :]
-
+#计算有效的剪裁尺寸
 def calculate_valid_crop_size(crop_size, upscale_factor):
     return crop_size - (crop_size % upscale_factor)
-
+#定一个一个来自文件夹的类 继承 data.Dataset
 class DatasetFromFolder(data.Dataset):
+    #本身 图像目录 旗帜 输入变形，目标变形 声明
     def __init__(self, image_dir, norm_flag, input_transform=None, target_transform=None, augment=False):
         super(DatasetFromFolder, self).__init__()
         # print(listdir(image_dir))
@@ -44,23 +45,32 @@ class DatasetFromFolder(data.Dataset):
         #         print(y)
         #print(join(image_dir, x))
         # self.image_filenames = [join(image_dir, x, x) for x in listdir(image_dir)]
+        #从一个图像文件夹下面读取 所有图片 sorted的目的是将图像排序
+        #生成列表里面包含了所有波段文件的名字
         self.image_filenames = [join(image_dir, x) for x in sorted(listdir(image_dir))]
         #self.image_filenames1 = [join(image_dir, x) for x in listdir(image_dir1)]
         print(self.image_filenames)
+        #将原始图像裂变打乱
         random.shuffle(self.image_filenames)
+        #输入图像名字
         print(self.image_filenames)
         #ToDo 确认这里是否需要随机打乱文件，由于不同光照的存在
+        #裁剪成和光谱波段数整数倍的大小
         self.crop_size = calculate_valid_crop_size(128, 4)
         self.input_transform = input_transform
         self.target_transform = target_transform
         self.augment = augment
         self.norm_flag = norm_flag
+    #允许对象以 a[index]的方式调用该函数
     def __getitem__(self, index):
         # input_image = load_img(self.image_filenames[index]) # 512 512 16
+        #按照传入index加载数据，加载特定波段的图像，这只能加载npy后缀的图片
         input_image  = np.load(self.image_filenames[index])
+        #把图像转化为32位的浮点数据类型
         input_image = input_image.astype(np.float32)
         if self.norm_flag:
             norm_name = 'maxnorm'
+            #得到这个图像像素的最大值
             max_raw = np.max(input_image)
             max_subband = np.max(np.max(input_image, axis=0), 0)
             norm_factor = max_raw / max_subband
